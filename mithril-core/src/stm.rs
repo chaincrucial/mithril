a48f4893becdabb7a7509640e62e5cdcadb26978
iquerejeta <querejeta.azurmendi@iohk.io>
2022-08-19 08:59:55 +0200

    New StmInitializer setup

--- mithril-core/src/stm.rs
+++ mithril-core/src/stm.rs
@@ -116,6 +116,8 @@ use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::convert::{From, TryFrom, TryInto};
 use std::hash::{Hash, Hasher};
+use kes_summed_ed25519::kes::{Sum6Kes, Sum6KesSig};
+use kes_summed_ed25519::traits::KesSk;
 
 /// The quantity of stake held by a party, represented as a `u64`.
 pub type Stake = u64;
@@ -160,6 +162,8 @@ pub struct StmInitializer {
     pub(crate) sk: SigningKey,
     /// Verification (public) key + proof of possession.
     pub(crate) pk: StmVerificationKeyPoP,
+    /// KES signature of `pk`
+    pub(crate) kes_sig: Option<Sum6KesSig>,
 }
 
 /// Participant in the protocol can sign messages.
@@ -261,6 +265,23 @@ impl StmParameters {
 
 impl StmInitializer {
     /// Builds an `StmInitializer` that is ready to register with the key registration service.
+    /// This function generates the signing and verification key with a PoP, signs the verification
+    /// key with a provided KES signing key, and initialises the structure.
+    pub fn setup_new<R: RngCore + CryptoRng>(params: StmParameters, kes_sk: &[u8], kes_period: usize, stake: Stake, rng: &mut R) -> Self {
+        let sk = SigningKey::gen(rng);
+        let pk = StmVerificationKeyPoP::from(&sk);
+        let kes_sk = Sum6Kes::from_bytes(kes_sk).expect("Invalid KES key provided"); // todo: handle this
+        let kes_sig = Some(kes_sk.sign(kes_period, &pk.to_bytes()));
+
+        Self {
+            stake,
+            params,
+            sk,
+            pk,
+            kes_sig
+        }
+    }
+    /// Builds an `StmInitializer` that is ready to register with the key registration service.
     /// This function generates the signing and verification key with a PoP, and initialises the structure.
     pub fn setup<R: RngCore + CryptoRng>(params: StmParameters, stake: Stake, rng: &mut R) -> Self {
         let sk = SigningKey::gen(rng);
@@ -270,6 +291,7 @@ impl StmInitializer {
             params,
             sk,
             pk,
+            kes_sig: None
         }
     }
 
@@ -343,6 +365,7 @@ impl StmInitializer {
             params,
             sk,
             pk,
+            kes_sig: None
         })
     }
 }
@@ -519,6 +542,7 @@ impl<D: Clone + Digest + FixedOutput> StmSigner<D> {
             params: self.params,
             pk: StmVerificationKeyPoP::from(&self.sk),
             sk: self.sk,
+            kes_sig: None
         }
     }
 
